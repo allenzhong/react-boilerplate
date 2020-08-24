dca61280fe2712e99912ba003b173c2c0dc0469e
allenzhong <zhong.allen@gmail.com>
2020-08-24 21:53:12 +1200

    Split code by using dynamic import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { hot } from 'react-hot-loader'
 
+const Warning = React.lazy(() => import('./Warning'))
+
 class App extends React.Component {
   state = {
     count: 0,
@@ -14,7 +16,11 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World</h1>
-        <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
+        <h2 className={count > 10 ? 
+        <React.Suspense fallback={null}>
+          <Warning /> 
+        </React.Suspense>
+        : null}>Count: {count}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
       </div>
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -30,6 +30,7 @@ module.exports = {
           plugins: [
             'react-hot-loader/babel',
             '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-syntax-dynamic-import'
           ]
         }
       },
