9372509db3363f77e6c2f1d6a07b0be36bb372b0
allenzhong <zhong.allen@gmail.com>
2020-08-25 22:19:59 +1200

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import { hot } from 'react-hot-loader'
 
 const Warning = React.lazy(() => import('./Warning'))
@@ -6,25 +6,31 @@ const Warning = React.lazy(() => import('./Warning'))
 class App extends React.Component {
   state = {
     count: 0,
-  };
+  }
 
-  increment = () => this.setState((state) => ({ count: state.count + 1 }));
-  decrement = () => this.setState((state) => ({ count: state.count - 1 }));
+  increment = () => this.setState((state) => ({ count: state.count + 1 }))
+  decrement = () => this.setState((state) => ({ count: state.count - 1 }))
 
   render() {
-    const { count } = this.state;
+    const { count } = this.state
     return (
       <div>
         <h1>Hello World</h1>
-        <h2 className={count > 10 ? 
-        <React.Suspense fallback={null}>
-          <Warning /> 
-        </React.Suspense>
-        : null}>Count: {count}</h2>
+        <h2
+          className={
+            count > 10 ? (
+              <React.Suspense fallback={null}>
+                <Warning />
+              </React.Suspense>
+            ) : null
+          }
+        >
+          Count: {count}
+        </h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
       </div>
-    );
+    )
   }
 }
 
